a3a128493c4f29359cd4ee2faa8b7fc7dc4b2147
abrar000 <abrar@uconnect.pk>
2019-01-10 17:05:53 +0500

    nature of bubbling

---
+++ bubbling/bubling.js
@@ -0,0 +1,11 @@
+
+
+document.querySelector('.one').addEventListener('click', function(){
+        console.log('one')
+      });
+      document.querySelector('.two').addEventListener('click', function(){
+        console.log('two')
+      },true);
+      document.querySelector('.three').addEventListener('click', function(){
+        console.log('three')
+      });
